2cdd1fbe3919e73b64b9b592c376c082cd2bb8df
askeing <askeing@gmail.com>
2016-04-12 13:43:36 +0800

    fix typo and add parameter p for remote query

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,36 @@
  * Created by Askeing on 16/4/11.
  */
 
+function getParameter(parameter_name)
+{
+    var selfURL = window.location.search.substring(1);
+    var parameters = selfURL.split('&');
+    for (var i = 0; i < parameters.length; i++)
+    {
+        var parameter = parameters[i].split('=');
+        if (parameter[0] == parameter_name)
+        {
+            return parameter[1];
+        }
+    }
+}
+
 $(document).ready(function() {
 
     var performance_json = new Object();
 
-
-    $.getJSON( "data.json", function( data ) {
-        $.each(data, function (key, val) {
-            performance_json[key] = val;
-        });
+    var p = getParameter("p");
+    if (p) {
+        performance_json = JSON.parse(decodeURI(p));
         paint(performance_json);
-    });
-
+    } else {
+        $.getJSON("data.json", function (data) {
+            $.each(data, function (key, val) {
+                performance_json[key] = val;
+            });
+            paint(performance_json);
+        });
+    }
 
     $("#btnPaint").click( function() {
         $("#helpBlock").text();
